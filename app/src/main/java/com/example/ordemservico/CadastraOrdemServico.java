ac2c8d60a973c10edfb5bbe6aefc57fb404bdd8d
Humberto <humbertoftneto>
2019-12-27 08:41:56 -0300

    Tela de cadastro da Ordem de Serviço.

--- app/src/main/java/com/example/ordemservico/CadastraOrdemServico.java
+++ app/src/main/java/com/example/ordemservico/CadastraOrdemServico.java
@@ -3,12 +3,18 @@ package com.example.ordemservico;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CadastraOrdemServico extends AppCompatActivity {
-
+EditText edt_numOS, edt_descricaoOS, edt_setorOS, edt_solicitanteOS, edt_observacoesOS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastra_ordem_servico);
+        edt_numOS = (EditText)findViewById(R.id.ed_numOrdemServico);
+        edt_descricaoOS = (EditText)findViewById(R.id.ed_descricaoOrdemServico);
+        edt_setorOS = (EditText)findViewById(R.id.ed_setorOrdemServico);
+        edt_solicitanteOS = (EditText)findViewById(R.id.ed_solicitanteOrdemServico);
+        edt_observacoesOS = (EditText)findViewById(R.id.ed_observacoesOrdemServico);
     }
 }

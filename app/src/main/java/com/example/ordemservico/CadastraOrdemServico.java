0becd90f8196dfcf6d7f9702b5fc854e534d6484
Humberto <humbertoftneto>
2020-01-03 16:10:17 -0300

    Abre tela de cadastro ao clicar em nova ordem de serviço.

--- app/src/main/java/com/example/ordemservico/CadastraOrdemServico.java
+++ app/src/main/java/com/example/ordemservico/CadastraOrdemServico.java
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class CadastraOrdemServico extends AppCompatActivity {
 EditText edt_numOS, edt_descricaoOS, edt_setorOS, edt_solicitanteOS, edt_observacoesOS;
+
+FirebaseDatabase firebaseDatabase;
+DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/ordemservico/MainActivity.java
+++ app/src/main/java/com/example/ordemservico/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ordemservico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.example.ordemservico.Model.OrdemServicoBEAN;
@@ -18,4 +21,32 @@ public class MainActivity extends AppCompatActivity {
         final ListView lista = (ListView) findViewById(R.id.lst_ordemservico);
         //final ArrayList <OrdemServicoBEAN> ordemservico = preencheDados();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+        case R.id.menu_novo:novaClick();
+
+            return(true);
+        case R.id.menu_executando:
+
+            return(true);
+        case R.id.menu_concluido:
+
+            return(true);
+            default:
+                return super.onOptionsItemSelected(item);
+    }
+    }
+    private void novaClick() {
+       Intent intent_nova = new Intent(this,CadastraOrdemServico.class);
+       intent_nova.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+       startActivity(intent_nova);
+   }
 }
